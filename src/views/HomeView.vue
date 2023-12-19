d8669a09476928b89ad65ecc6e603c0dea9c3a70
Ashenafee <ashenafeem@gmail.com>
2023-12-18 22:17:50 -0500

    Use daisyUI over Primevue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,222 +1,196 @@
+<template>
+    <div class="flex flex-col w-full h-screen justify-center items-center overflow-y-auto">
+        <div class="flex flex-col w-1/2">
+            <h1 class="text-4xl font-bold mb-2">CiteThat</h1>
+
+            <!-- Search -->
+            <div class="flex flex-row w-full justify-center items-center">
+                <input v-model="inputText" type="text" placeholder="PubMed Article" class="input input-bordered w-full rounded-r-none focus:outline-none" />
+                <button class="btn bg-blue-700 hover:bg-blue-800 rounded-l-none" @click="onClickCite">
+                    Search
+                </button>
+            </div>
+
+            <!-- Citation Modal -->
+            <dialog id="citationModal" class="modal" visible="visibleCitation">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg">
+                        Citation
+                    </h3>
+                    <p class="py-4">{{ citation }}</p>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+
+            <!-- Source Modal -->
+            <dialog id="sourceModal" class="modal" visible="visibleSource">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg">
+                        Source
+                    </h3>
+                    
+                    <div class="flex flex-row w-full justify-center items-center">
+                        <input v-model="articleSourceUrl" type="text" placeholder="Source" class="input input-bordered w-full rounded-r-none focus:outline-none" />
+                        <button class="btn bg-blue-700 hover:bg-blue-800 rounded-l-none" @click="onClickSetSource">
+                            Set
+                        </button>
+                    </div>
+                </div>
+                <form method="dialog" class="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+
+            <progress v-if="progressBarVisible" class="progress w-full h-2 mt-2"></progress>
+            <!-- Results Portion -->
+            <div v-if="resultsVisible" class="mt-2">
+                <!-- Result -->
+                <div class="collapse collapse-arrow bg-base-200">
+                    <input type="checkbox" id="result" class="collapse-toggle" />
+                    <label for="result" class="collapse-title text-xl font-medium cursor-pointer">
+                        {{ articleStore.article.title }}
+                    </label>
+                    <div class="collapse-content">
+                        <p class="mb-2 overflow-y-auto h-48">
+                            {{ articleStore.article.abstract }}
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Buttons -->
+                <div class="mt-2 flex flex-row w-full space-x-2">
+                    <button class="btn flex-1 bg-blue-700 hover:bg-blue-800" @click="onClickGetCitation">
+                        Cite
+                    </button>
+                    <button class="btn flex-1 bg-blue-700 hover:bg-blue-800" @click="onClickDownload">
+                        Download
+                    </button>
+                    <button class="btn flex-1 bg-blue-700 hover:bg-blue-800" @click="onClickView">
+                        View
+                    </button>
+                </div>
+            </div>
+            <button class="btn bg-blue-700 hover:bg-blue-800 mt-2" @click="onClickOpenSourceModal">
+                Source
+            </button>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
-
 import { ref } from "vue";
+import { articleStore } from "@/stores/article";
+import { fetchArticle, parsePmid } from "@/composables/useArticle";
+import { generateCitation } from "@/utils/cite";
 
-const visible = ref(false);
-const visibleCitation = ref(false);
+const inputText = ref('');
+const progressBarVisible = ref(false);
+const resultsVisible = ref(false);
 const citation = ref('');
+const visibleCitation = ref(false);
+const visibleSourceDialog = ref(false);
+const articleSourceUrl = ref('');
 
-import { articleStore } from '@/stores/article';
-
-const onClickCite = () => {
-
-  // Grab the text from the PubMed URL input field
-  const inputTextElement = document.getElementById("pubmed-search") as HTMLInputElement;
-  const inputText = inputTextElement.value;
-
-  if (!inputText) {
-    alert('Please enter a PubMed URL.');
-    return;
-  }
-  
-  const resultsElement = document.getElementById('pubmed-results') as HTMLDivElement;
-
-  // Grab the progress bar and begin it
-  const progressBar = document.getElementById('pubmed-progress') as HTMLProgressElement;
-  progressBar.classList.replace('invisible', 'visible');
-
-  // Parse the ID from the PubMed URL
-  const pmid = parsePmid(inputText);
-
-  // Call the Entrez API
-  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${pmid}`;
-  fetch(url)
-    .then((response) => response.text())
-    .then((data) => {
-      const content = new window.DOMParser().parseFromString(data, "text/xml");
-
-      // Parse the response
-      const doi = content.querySelector("PubmedArticleSet > PubmedArticle > PubmedData > ArticleIdList > ArticleId[IdType='doi']")?.textContent;
-
-      // Query using Crossref for information on the article
-      if (doi) {
-        fetch(`https://api.crossref.org/works/${doi}`)
-          .then((response) => response.json())
-          .then((data) => {
-            progressBar.classList.replace('visible', 'invisible');
-            console.log(data);
-
-            articleStore.parseToArticle(data.message);
-            console.log(articleStore.article);
-          });
-
-      } else {
+/**
+ * Handles the click event for the "Cite" button. If the input text is empty,
+ * displays an alert message. Otherwise, fetches the article data using the
+ * PubMed URL, sets the fetched article in the article store, and displays the
+ * results. If an error occurs during fetching, displays an error message.
+ * Finally, hides the progress bar.
+ */
+const onClickCite = async () => {
+    if (!inputText.value) {
+        alert('Please enter a PubMed URL.');
         return;
-      }
-    })
-    .then((data) => {
-      console.log(data);
-      resultsElement.classList.replace('invisible', 'visible');
-      resultsElement.classList.add('flex', 'flex-col', 'w-full');
-    })
-
-  console.log(pmid);
-};
-
-const parsePmid = (url: string) => {
-
-  const splitUrl = url.split('/');
-
-  const elements: string[] = [];
-
-  splitUrl.forEach((x) => {
-    if (x) {
-      elements.push(x);
     }
-  });
-
-  return elements.pop();
-};
-
-const onClickGetCitation = () => {
-
-  // Get the authors
-  let authors = '';
-  if (articleStore.article.authors) {
-    articleStore.article.authors.forEach((author) => {
-      authors += `${author.given} ${author.family}, `;
-    });
-    authors = authors.slice(0, -2);
-  }
-
-  const articleTitle = articleStore.article.title;
-  const journalTitle = articleStore.article.journal;
-  const year = articleStore.article.year;
-  const volume = articleStore.article.volume;
-  const issue = articleStore.article.issue;
-  const pages = articleStore.article.pages;
-
-  // Set the citation
-  citation.value = `${authors}. ${articleTitle}. ${journalTitle}. ${year};${volume}(${issue}):${pages}.`;
-
-  // Open the dialog
-  visibleCitation.value = true;
-};
-
-const onClickDownload = () => {
-  // Get the DOI
-  const doi = articleStore.article.doi;
-
-  // Get the source URL from local storage
-  let source = localStorage.getItem('article-source');
-
-  if (source) {
-
-    // Replace the {doi} with the DOI
-    if (doi) {
-      source = source.replace('{doi}', doi);
+    progressBarVisible.value = true;
+    try {
+        const pmid = parsePmid(inputText.value);
+        const article = await fetchArticle(pmid);
+        articleStore.setArticle(article);
+        resultsVisible.value = true;
+    } catch (error) {
+        console.error(error);
+        alert('Error fetching article data.');
+    } finally {
+        progressBarVisible.value = false;
     }
-    
-    // Open the URL in a new tab
-    window.open(source, '_blank');
-    return;
-  } else {
-    // Alert the user that the source URL is not set
-    alert('The source URL is not set. Please set the source URL using the "Source" button.');
-  }
 };
 
-const onClickView = () => {
-  // Get the DOI
-  const doi = articleStore.article.doi;
+const onClickGetCitation = async () => {
+    try {
+        const fetchedCitation = generateCitation(articleStore.article, 'CSE');
+        if (fetchedCitation !== void 0) {
+            citation.value = fetchedCitation;
+        } else {
+            citation.value = '';
+        }
+        visibleCitation.value = true;
 
-  // Open the URL in a new tab
-  window.open(`https://doi.org/${doi}`, '_blank');
+        // Show modal
+        const modal = document.getElementById('citationModal') as HTMLDialogElement;
+        modal.showModal();
+    } catch (error) {
+        console.error(error);
+        alert('Error generating citation.');
+    }
+};
+
+const onClickOpenSourceModal = () => {
+    // Show modal
+    const modal = document.getElementById('sourceModal') as HTMLDialogElement;
+    modal.showModal();
 }
 
 const onClickSetSource = () => {
-  // Get the URL
-  const url = document.getElementById('article-source-url') as HTMLInputElement;
+    localStorage.setItem('article-source', articleSourceUrl.value);
+    visibleSourceDialog.value = false;
 
-  // Set the source in local storage
-  localStorage.setItem('article-source', url.value);
+    // Hide modal
+    const modal = document.getElementById('sourceModal') as HTMLDialogElement;
+    modal.close();
+};
 
-  // Close the dialog
-  visible.value = false;
-}
+const onClickDownload = () => {
+    const source = localStorage.getItem('article-source');
+    if (!source) {
+        alert('Please set article source URL.');
+        return;
+    }
+    const url = `${source}${articleStore.article.doi}`;
+    window.open(url, '_blank');
+    console.log("🚀 ~ file: HomeView.vue:119 ~ onClickDownload ~ url:", url)
+};
 
-
+const onClickView = () => {
+    const source = localStorage.getItem('article-source');
+    if (!source) {
+        alert('Please set article source URL.');
+        return;
+    }
+    const url = `https://doi.org/${articleStore.article.doi}`;
+    window.open(url, '_blank');
+};
 </script>
-
-<template>
-  <div class="flex flex-col w-full h-screen justify-center items-center overflow-y-auto">
-    <div class="flex flex-col w-1/2">
-      <h1 class="text-4xl font-bold mb-2">CiteThat</h1>
-
-      <div class="flex flex-row w-full justify-center items-center">
-        <InputText id="pubmed-search" placeholder="PubMed Article"
-          class="border-0 rounded-tl-xl rounded-bl-xl rounded-tr-none rounded-br-none w-full" />
-        <Button class="border-0 rounded-tr-xl rounded-br-xl rounded-tl-none rounded-bl-none focus:shadow-none"
-          v-on:click="onClickCite">Cite</Button>
-      </div>
-      
-      <Button class="border-0 mt-2 rounded-xl focus:shadow-none" label="Source" icon="pi pi-external-link" @click="visible = true" />
-
-      <Dialog v-model:visible="visible" modal header="Source" :style="{ width: '50vw' }">
-        <div class="flex flex-row w-full justify-center items-center">
-          <InputText id="article-source-url" placeholder="Set article source URL..."
-          class="border-0 rounded-xl w-full my-2 focus:shadow-none" />
-          <Button severity="info" class="border-0 rounded-tr-xl rounded-br-xl rounded-tl-none rounded-bl-none focus:shadow-none"
-          v-on:click="onClickSetSource">Set</Button>
-        </div>
-      </Dialog>
-
-      <Dialog v-model:visible="visibleCitation" modal header="CSE Citation" :style="{ width: '50vw' }">
-        <div class="flex flex-row w-full justify-center items-center">
-          <p class="text-lg font-bold mb-2">{{ citation }}</p>
-        </div>
-      </Dialog>
-
-      <ProgressBar id="pubmed-progress" mode="indeterminate" class="h-2 mt-2 rounded-xl invisible"></ProgressBar>
-      <div id="pubmed-results" class="invisible">
-        <Accordion>
-          <!-- Fetch the article title from the store -->
-          <AccordionTab :header="articleStore.article.title">
-            <h4 class="text-lg font-bold mb-2">Abstract</h4>
-            <p class="mb-2 overflow-y-auto h-48">{{ articleStore.article.abstract }}</p>
-          </AccordionTab>
-        </Accordion>
-
-        <div class="mt-2 flex flex-row w-full">
-          <Button severity="info" class="mr-2 border-0 rounded-xl focus:shadow-none w-1/2 justify-center" v-on:click="onClickGetCitation">Cite</Button>
-          <Button severity="info" class="mr-2 border-0 rounded-xl focus:shadow-none w-1/2 text-center justify-center"
-            v-on:click="onClickDownload">Download</Button>
-          <Button severity="info" class="focus:shadow-none border-0 rounded-xl w-1/2 text-center justify-center"
-            v-on:click="onClickView">View</Button>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 
 <style scoped>
 :deep(.p-inputtext:focus) {
-  box-shadow: none;
+    box-shadow: none;
 }
 
 :deep(.p-accordion-header:focus) {
-  box-shadow: none;
-  outline: none;
+    box-shadow: none;
+    outline: none;
 }
 
 :deep(#pubmed-results > div.mt-2.flex.flex-row.w-full > .p-button:focus) {
-  box-shadow: none;
-  outline: none;
+    box-shadow: none;
+    outline: none;
 }
 
-/* Apply box-shadow none to all "a" elements that are children of p-accordion-header elements when focused */
 :deep(#pubmed-results > div > div > div.p-accordion-header > a:focus) {
-  box-shadow: none;
+    box-shadow: none;
 }
 </style>
